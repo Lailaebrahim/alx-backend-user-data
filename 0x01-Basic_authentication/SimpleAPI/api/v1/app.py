8d556f600aae2d8b4969adb54cef7c8205e977db
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-03 06:35:23 +0300

    Task 1

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
   
   
 @app.errorhandler(401)
-def not_authorized(error) -> str:
+def unauthorized(error) -> str:
     """ Not authorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
