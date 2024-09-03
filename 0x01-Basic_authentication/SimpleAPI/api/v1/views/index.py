83d5bc502bdf52c57e25c368d1271b85e2c98e1d
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-03 06:36:27 +0300

    Task 1

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -26,10 +26,11 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized() -> None:
     """ GET /api/v1/unauthorized
     Return:
       - 401 status code
     """
     abort(401)
+  
