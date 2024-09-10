a3ed48c9c9794de911b490af8af9710c6a32cad9
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-10 23:39:29 +0300

    Task 0: Define user model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """Define User Model"""
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, VARCHAR
 
 
 class User():
     """ User Model"""
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=False)
-    reset_token = Column(String(250), nullable=True)
+    email = Column(VARCHAR(250), nullable=False)
+    hashed_password = Column(VARCHAR(250), nullable=False)
+    session_id = Column(VARCHAR(250), nullable=False)
+    reset_token = Column(VARCHAR(250), nullable=True)
