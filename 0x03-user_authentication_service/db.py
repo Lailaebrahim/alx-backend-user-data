5164434ca5541ca2b5bff514cc48d46911729c01
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-11 00:01:02 +0300

    Task 1: complete db class to add method to add user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from user import User
 from user import Base
 
 
@@ -29,10 +29,9 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add a user to the database
         """
-        from user import User
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
