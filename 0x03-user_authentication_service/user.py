4b2ee359e9ce4f85e87332d186da2fb74de1b2fc
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-11 00:05:05 +0300

    Task 1: complete db class to add method to add user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Define User Model"""
-from sqlalchemy import Column, Integer, VARCHAR
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -9,8 +9,8 @@ Base = declarative_base()
 class User(Base):
     """ User Model"""
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(VARCHAR(250), nullable=False)
-    hashed_password = Column(VARCHAR(250), nullable=False)
-    session_id = Column(VARCHAR(250), nullable=True)
-    reset_token = Column(VARCHAR(250), nullable=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
