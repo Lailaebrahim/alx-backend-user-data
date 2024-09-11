639cd1ae494a67b8f9e01c0e5cc65ff36d862a4d
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-12 02:23:14 +0300

    Task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -98,3 +98,15 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """ Destroy session associated with the user_id"""
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Send Reset Token """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                reset_token = _generate_uuid()
+                user.reset_token = reset_token
+                self._db._session.commit()
+                return reset_token
+        except NoResultFound:
+            raise ValueError
