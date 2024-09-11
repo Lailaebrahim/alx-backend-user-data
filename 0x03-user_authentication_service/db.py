cbe5faee6853b93858da4e7d43ac199d55ba8f89
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-11 22:09:49 +0300

    Task 2: complete db class : add find_user_by method to find user by any argument fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,6 +1,6 @@
 """DB module
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
@@ -49,10 +49,20 @@ class DB:
     
     def find_user_by(self, **kwargs: dict) -> User:
         """ Find user by a given argument"""
-        for key in kwargs.keys():
-            if not hasattr(User, key):
-                raise InvalidRequestError
-        user = self._session.query(User).filter_by(**kwargs).one()
-        if user is None:
-            raise NoResultFound
-        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user based on a set of filters.
+        """
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
