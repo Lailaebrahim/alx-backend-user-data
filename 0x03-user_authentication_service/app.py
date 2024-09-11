ce9a49f8be1c8f1813275ab4cfe9ae4e1a7b553a
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-12 01:23:04 +0300

    Task 11: add login route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -35,14 +35,15 @@ def login():
     try:
         if AUTH.valid_login(email, password):
             session_id = AUTH.create_session(email)
-            response = make_response(jsonify({"email": email, "message": "logged in"}), 200)
+            response = make_response(jsonify({"email": email,
+                                              "message": "logged in"}), 200)
             response.set_cookie("session_id", session_id)
-            return  response
+            return response
         else:
             abort(401)
     except Exception:
-        return jsonify({"message": "Internal Server Erro"}, 500)   
-    
+        return jsonify({"message": "Internal Server Erro"}, 500)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
