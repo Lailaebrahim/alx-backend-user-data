82a988fa70204bdf0961be4b53b6b62ccc6e1d22
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-12 01:30:59 +0300

    Task 11: add login route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -33,11 +33,11 @@ def login():
     email = request.form.get('email')
     password = request.form.get('password')
     if AUTH.valid_login(email, password):
-            session_id = AUTH.create_session(email)
-            response = make_response(jsonify({"email": email,
-                                              "message": "logged in"}), 200)
-            response.set_cookie("session_id", session_id)
-            return response
+        session_id = AUTH.create_session(email)
+        response = make_response(jsonify({"email": email,
+                                            "message": "logged in"}), 200)
+        response.set_cookie("session_id", session_id)
+        return response
     else:
         abort(401)
 
