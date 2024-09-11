2f1eb004deeefdd64b86f939f81870dc06a726a6
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-11 23:43:11 +0300

    Task 6: add register users post route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """Flask App"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -9,6 +13,20 @@ app = Flask(__name__)
 def home():
     """Home route"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_user():
+    payload = request.get_json()
+    email = payload.get("email")
+    password = payload.get("password")
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    except Exception as e:
+        return jsonify(e)
+    return jsonify({"message": "User registered successfully"})
 
 
 if __name__ == "__main__":
