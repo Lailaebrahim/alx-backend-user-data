b1db90b2b802f00503a8a31a92e535f7bc77113a
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-11 23:36:24 +0300

    Task 6: create a simple flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
-def home(request):
+def home():
     """Home route"""
     return jsonify({"message": "Bienvenue"})
 
