cf06589ade7286ced37c35ab49f98ad89beccd32
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-11 22:08:14 +0300

    Task 2: complete db class : add find_user_by method to find user by any argument

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -52,9 +52,7 @@ class DB:
         for key in kwargs.keys():
             if not hasattr(User, key):
                 raise InvalidRequestError
-            
-        try:
-            user = self._session.query(User).filter_by(**kwargs).one()
-        except NoResultFound:
+        user = self._session.query(User).filter_by(**kwargs).one()
+        if user is None:
             raise NoResultFound
         return user
