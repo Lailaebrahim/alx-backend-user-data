c6670f2ace578d5e2179cb8313a72d49e1845742
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-12 23:22:36 +0300

    Advanced Task Integration test

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,17 +1,83 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-from auth import Auth
+"""Integration test"""
+import requests
 
-email = 'bob@bob.com'
-password = 'MyPwdOfBob'
-auth = Auth()
 
-auth.register_user(email, password)
+def register_user(email: str, password: str) -> None:
+    """Register a new user"""
+    url = 'http://localhost:5000/users'
+    data = {'email': email, 'password': password}
+    response = requests.post(url, data=data)
+    assert response.status_code == 200
 
-print(auth.valid_login(email, password))
 
-print(auth.valid_login(email, "WrongPwd"))
+def log_in_wrong_password(email: str, password: str) -> None:
+    """ Log in with wrong credentails """
+    url = "http://localhost:5000/sessions"
+    data = {'email': email, 'password': password}
+    response = requests.post(url, data)
+    assert response.status_code == 401
 
-print(auth.valid_login("unknown@email", password))
+
+def log_in(email: str, password: str) -> str:
+    """ Log in with the right credentails"""
+    url = "http://localhost:5000/sessions"
+    data = {'email': email, 'password': password}
+    response = requests.post(url, data)
+    assert response.status_code == 200
+    return response.cookies.get('session_id')
+
+def profile_unlogged() -> None:
+    """ test  request profile when not logged in"""
+    url = "http://localhost:5000/profile"
+    response = requests.get(url)
+    assert response.status_code == 403
+
+
+def profile_logged(session_id: str) -> None:
+    """ test request profile when logged in"""
+    url = "http://localhost:5000/profile"
+    cookies = {'session_id': session_id}
+    response = requests.get(url, cookies=cookies)
+    assert response.status_code == 200
+
+
+def log_out(session_id: str) -> None:
+    """ test log out route"""
+    url = "http://localhost:5000/sessions"
+    cookies = {'session_id': session_id}
+    response = requests.delete(url, cookies=cookies)
+    assert response.status_code == 302
+
+
+def reset_password_token(email: str) -> str:
+    """test reset token route"""
+    url = "http://localhost:5000/reset_password"
+    data = {"email": email}
+    response = requests.post(url, data)
+    assert response.status_code == 200
+    return response.json().get('reset_token')
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """test reset password route"""
+    url = "http://localhost:5000/reset_password"
+    data = {"email": email, "reset_token": reset_token, "new_password": new_password}
+    response = requests.put(url, data)
+    assert response.status_code == 200
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+if __name__ == "__main__":
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
