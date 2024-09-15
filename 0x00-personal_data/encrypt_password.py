38e64880129ad4d2685d21480d6f91dbd5946d2c
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-15 22:35:18 +0300

    Task 5 & 6

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Password Hashing"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Takes in a password string arguments and
+    returns a salted, hashed password"""
+    return bcrypt.hashpw(password.encode('utf-8'),
+                         bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check hashed passwords"""
+    return bcrypt.checkpw(bcrypt.hashpw(password.encode('utf-8'),
+                                        bcrypt.gensalt()), hashed_password)
