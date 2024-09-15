8cdbd38fbff9a180d0e0ac8b9f0e9fe3dc528161
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-15 20:51:40 +0300

    Task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Method to filter logging
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    filter logging
+    """
+    for field in fields:
+        message = re.sub(rf'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
